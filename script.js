be27c69eb55f122a67dda53e1c5f74caaa523039
Petr Gondek <petrgon@gmail.com>
2022-09-09 13:07:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.dndbeyond.com/*
 // @grant       none
-// @version     1.4
+// @version     2.0
 // @author      Petr Gondek
 // @description Adds a button to DnDBeyond to filter the content you own.
 // @license MIT
