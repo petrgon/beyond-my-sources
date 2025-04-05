e17950c6049908fdc3e95e2165edc44a2b22c8b7
Kelvin Lin <lin.kelvin11@gmail.com>
2025-04-05 16:03:05 -0400

    update script version

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.dndbeyond.com/*
 // @grant       none
-// @version     2.3.2
+// @version     2.3.3
 // @author      Petr Gondek
 // @description Adds a button to DnDBeyond to filter sources you own.
 // @license MIT
